8f332b2851650dc9f7269a5fd243fe57187442ed
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2021-07-29 17:52:04 +0200

    unet

---
+++ src/unet.jl
@@ -0,0 +1,84 @@
+struct CropCat
+    trms
+end
+
+function (cc::CropCat)(x1, x2) # x2 : input
+    lo = cc.trms .+ 1
+    up = size(x2) .- cc.trms
+    chcat(x2[UnitRange.(lo, up)...], x1)
+end
+
+utrim(i) = (192 - 2^(i + 3)) ÷ 2^i
+"""
+    unet(; inchannels)
+Build a [U-Net](https://arxiv.org/abs/1505.04597v1) to process images with 
+`inchannels` channels (`inchannels` = 3 for RGB images).
+"""
+function unet(; inchannels)
+    function uconv(ch)
+        ich, och = ch
+        [Conv((3, 3), ich=>och, relu), Conv((3, 3), och=>och, relu)]
+    end
+    
+    udown() = MaxPool((2, 2))
+    
+    uup(ch) = ConvTranspose((2, 2), ch, relu, stride = (2, 2))
+    
+    function downblock(ich)
+        och = 2 * ich
+        [udown(), uconv(ich=>och)...]
+    end
+    
+    function upblock(ich)
+        mch = ich ÷ 2
+        och = mch ÷ 2
+        [uconv(ich=>mch)..., uup(mch=>och)]
+    end
+
+    enc = Any[uconv(inchannels=>64)]
+    dec = Any[Chain(uconv(128=>64)..., Conv((1, 1), 64=>1, sigmoid))]
+    dch = 64
+    uch = 256
+    for l ∈ 1:3
+        push!(enc, downblock(dch))
+        push!(dec, upblock(uch))
+        dch *= 2
+        uch *= 2
+    end
+    b = Chain(udown(), uconv(512=>1024)..., uup(1024=>512))
+    con = []
+    for l ∈ 1:4
+        t = utrim(l)
+        push!(con, CropCat((t, t)))
+    end
+    uchain(encoders = enc, decoders = dec, bridge = b, connection = con)
+end
+
+const uminsize = 204
+
+#=
+Return a tuple of padding values for both input image and ground truth image.
+is : input size
+d : U-Net depth (default 4, as in original implementation)
+nc : number of unpadded convolution per U-block (default 2, as in original implementation)
+
+using Images, ImageIO
+
+ip, op = upadding((256, 256))
+pimg = padarray(img, Pad(:reflect, ip...))
+pgth = padarray(gth, Pad(:reflect, op...))
+
+=#
+function upadding(is)
+    tr = utrim(1)
+    
+    function newsize(is)
+        n = is + 2 * tr + 4 * nc
+        k = ceil(Int, (n - uminsize) / 16)
+        ms + k * 16
+    end
+    
+    pa = (newsize.(is) .- is) ./ 2
+    os = (floor.(Int, pa), ceil.(Int, pa))
+    os, ([o .- tr .- 2 * nc for o ∈ os]...,)
+end
---
+++ src/utils.jl
@@ -0,0 +1,95 @@
+"""
+    chcat(x...)
+
+Concatenate the image data along the dimension corresponding to the channels.
+Image data should be stored in WHCN order (width, height, channels, batch) or
+WHDCN (width, height, depth, channels, batch) in 3D context. Channels are
+assumed to be the penultimate dimension.
+
+# Example
+```jldoctest
+julia> x1 = rand(32, 32, 4, 6); # a batch of 6 images (32x32) with 4 channels
+
+julia> x2 = rand(32, 32, 4, 6); # a batch of 6 images (32x32) with 4 channels
+
+julia> chcat(x1, x2) |> size
+(32, 32, 8, 6)
+```
+"""
+chcat(x...) = cat(x...; dims = (x[1] |> size |> length) - 1)
+
+"""
+    uchain(;encoders, decoders, bridge, connection)
+
+Build a `Chain` with U-Net like architecture. `encoders` and `decoders` are 
+arrays of encoding/decoding blocks, from top to bottom (see diagram below). 
+`bridge` is the bottom part of U-Net  architecture.
+Each level of the U-Net is connected through a 2-argument callable `connection`.
+`connection` could be an array in case the way levels are connected vary from 
+one level to another.
+
+Notes :
+- usually encoder blocks start with a 'MaxPool' to downsample image by 2, except
+the first encoder.
+- usually decoder blocks end with a 'ConvTranspose' to upsample image by 2,
+except the first decoder.
+
+```
++---------+                                                          +---------+
+|encoder 1|                                                          |decoder 1|
++---------+                                                          +---------+
+     |------------------------------------------------------------------->^     
+     |                                                                    |     
+     |   +---------+                                         +---------+  |     
+     +-->|encoder 2|                                         |decoder 2|--+     
+         +---------+                                         +---------+        
+              |-------------------------------------------------->^             
+              |                                                   |             
+              |   +---------+                        +---------+  |             
+              +-->|encoder 3|                        |decoder 3|--+             
+                  +---------+                        +---------+                
+                       |--------------------------------->^                     
+                       |                                  |                     
+                       |   +---------+       +---------+  |                     
+                       +-->|encoder 4|       |decoder 4|--+                     
+                           +---------+       +---------+                        
+                                |---------------->^                             
+                                |                 |                             
+                                |    +-------+    |                             
+                                +--->|bridge |----+                             
+                                     +-------+                                  
+```
+See also [`chcat`](@ref).
+"""
+function uchain(;encoders, decoders, bridge, connection)
+    length(encoders) == length(decoders) ||
+        throw(ArgumentError(
+            "The number of encoders should be equal to the number of decoders."))
+
+    if isa(connection, AbstractArray)
+        length(encoders) == length(connection) ||
+            throw(ArgumentError(
+                "The number of connections should be equal to the number of encoders/decoders."))
+    else
+        connection = repeat([connection], length(encoders))
+    end
+    
+    connect(enc::Chain, prl, dec::Chain) = Chain(enc..., prl, dec...)
+    
+    connect(enc::AbstractArray, prl, dec::AbstractArray) = Chain(enc..., prl, dec...)
+    
+    connect(enc::Chain, prl, dec::AbstractArray) = Chain(enc..., prl, dec...)
+    
+    connect(enc::AbstractArray, prl, dec::Chain) = Chain(enc..., prl, dec...)
+
+    connect(enc, prl, dec) = Chain(enc, prl, dec)
+
+    ite = zip(reverse(encoders[2:end]), reverse(decoders[2:end]), 
+              reverse(connection[1:(end - 1)]))
+    l = SkipConnection(bridge, connection[end])
+    for (e, d, c) ∈ ite
+        l = connect(e, l, d)
+        l = SkipConnection(l, c)
+    end
+    connect(encoders[1], l, decoders[1])
+end
