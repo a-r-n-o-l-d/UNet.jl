2dd4c1de6c02f0882278dab8015dffdac0a32cf3
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2021-08-31 12:19:14 +0200

    house keeping

--- src/unet.jl
+++ src/unet.jl
@@ -2,13 +2,23 @@ struct CropCat
     trms
 end
 
+function CropCat(t, vol)
+    #t = utrim(lvl)
+    if vol
+        t = (t, t, t)
+    else
+        t = (t, t)
+    end
+    CropCat((t..., 0, 0))
+end
+
 function (cc::CropCat)(x1, x2) # x2 : input
     lo = cc.trms .+ 1
     up = size(x2) .- cc.trms
     chcat(x2[UnitRange.(lo, up)...], x1)
 end
 
-utrim(i) = (192 - 2^(i + 3)) ÷ 2^i
+utrim(l) = (192 - 2^(l + 3)) ÷ 2^l
 
 """
     unet(; inchannels)
@@ -16,8 +26,72 @@ Build a [U-Net](https://arxiv.org/abs/1505.04597v1) to process images with
 `inchannels` channels (`inchannels` = 3 for RGB images). This implementation 
 corresponds to original paper with unpadded convolutions.
 """
-function unet(; inchannels) # 3D
-    function uconv(ch)
+function unet(; inchannels, volume = false) # 3D
+    chd = 64
+    chu = 2 * chd
+    kc, kd, ku, kr = (3, 3), (2, 2), (2, 2), (1, 1) #outside
+    if volume
+        kc = (kc..., 3)
+
+    end
+
+    conv(chs) = Conv(kc, chs, relu)
+
+    function conv2(chs)# vol, outside
+        #k = vol ? (3, 3, 3) : (3, 3)
+        #conv(chs) = Conv(k, chs, relu)
+        ic, oc = chs
+        conv(ic=>oc), conv(oc=>oc)
+    end
+
+    downsample() = MaxPool(kd) #k = vol ? (2, 2, 2) : (2, 2)
+
+    function upsample(ic)#vol, outside
+        #k = vol ? (2, 2, 2) : (2, 2)
+        oc = ic ÷ 2
+        ConvTranspose(k, ic=>oc, stride = k)
+    end
+
+    function encblock(chs, lvl)
+        blk = []
+        if lvl > 1
+            push!(blk, downsample())
+        end
+        push!(blk, conv2(chs)...)
+        blk
+    end
+
+    function decblock(chs, lvl)
+        ic, oc = chs
+        blk = []
+        push!(blk, conv2(chs)...)
+        if lvl == 1
+            push!(blk, Conv(kr, oc=>1, sigmoid))
+        else
+            push!(blk, upsample(ic))
+        end
+        blk
+    end
+
+    enc, dec, con = [], [], []
+    ice, oce = inchannels, 64 # number of input/ouput channels for encoder block
+    icd, ocd = 2 * oce, oce   # number of input/ouput channels for decoder block
+    for l ∈ 1:4
+        push!(enc, encblock(ice=>oce, l))
+        ice = oce
+        oce *= 2
+
+        push!(dec, decblock(icd=>ocd, l))
+        icd *= 2
+        ocd *= 2
+
+        push!(con, CropCat(utrim(l), ndim)) # Center-crop concatenator
+    end
+    bdg = Chain(downsample(), conv2(oce=>icd)..., upsample(icd)) # bridge
+
+    uchain(encoders = enc, decoders = dec, bridge = bdg, connection = con)
+
+ #=   function uconv(ch)
         ich, och = ch
         [Conv((3, 3), ich=>och, relu), Conv((3, 3), och=>och, relu)]
     end
@@ -54,6 +128,7 @@ function unet(; inchannels) # 3D
         push!(con, CropCat((t, t, 0, 0)))
     end
     uchain(encoders = enc, decoders = dec, bridge = b, connection = con)
+    =#
 end
 
 const uminsize = 204
--- src/utils.jl
+++ src/utils.jl
@@ -29,10 +29,10 @@ Each level of the U-Net is connected through a 2-argument callable `connection`.
 one level to another.
 
 Notes :
-- usually encoder blocks start with a 'MaxPool' to downsample image by 2, except
-the first encoder.
-- usually decoder blocks end with a 'ConvTranspose' to upsample image by 2,
-except the first decoder.
+- usually encoder block starts with a 'MaxPool' to downsample image by 2, except
+the first level encoder.
+- usually decoder block ends with a 'ConvTranspose' to upsample image by 2,
+except the first level decoder.
 
 ```
 +---------+                                                          +---------+
@@ -55,26 +55,24 @@ except the first decoder.
                            +---------+       +---------+                        
                                 |---------------->^                             
                                 |                 |                             
-                                |    +-------+    |                             
-                                +--->|bridge |----+                             
-                                     +-------+                                  
+                                |    +--------+   |                             
+                                +--->| bridge |---+                             
+                                     +--------+                                  
 ```
 See also [`chcat`](@ref).
 """
 function uchain(;encoders, decoders, bridge, connection)
-    length(encoders) == length(decoders) ||
-        throw(ArgumentError(
-            "The number of encoders should be equal to the number of decoders."))
+    length(encoders) == length(decoders) || throw(ArgumentError(
+        "The number of encoders should be equal to the number of decoders."))
 
     if isa(connection, AbstractArray)
-        length(encoders) == length(connection) ||
-            throw(ArgumentError(
-                "The number of connections should be equal to the number of encoders/decoders."))
+        length(encoders) == length(connection) || throw(ArgumentError(
+            "The number of connections should be equal to the number of encoders/decoders."))
     else
         connection = repeat([connection], length(encoders))
     end
     
-    connect(enc::Chain, prl, dec::Chain) = Chain(enc..., prl, dec...)
+    connect(enc::Chain, prl, dec::Chain) = Chain(enc..., prl, dec...) # uconnect  et en-dehors de uchain, ajout Any
     
     connect(enc::AbstractArray, prl, dec::AbstractArray) = Chain(enc..., prl, dec...)
     
@@ -84,7 +82,8 @@ function uchain(;encoders, decoders, bridge, connection)
 
     connect(enc, prl, dec) = Chain(enc, prl, dec)
 
-    ite = zip(reverse(encoders[2:end]), reverse(decoders[2:end]), 
+    ite = zip(reverse(encoders[2:end]),
+              reverse(decoders[2:end]), 
               reverse(connection[1:(end - 1)]))
     l = SkipConnection(bridge, connection[end])
     for (e, d, c) ∈ ite
